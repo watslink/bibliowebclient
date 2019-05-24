c09b3193dcacdf631b62d9476873c63f6ab6928d
watslink <seb.decroix@gmail.com>
2019-05-24 15:31:06 +0200

    creation de BorrowingController + mapping borrowings.jsp

---
+++ bibliowebclient-webapp/src/main/java/com/sd/oc/webapp/controller/BorrowingController.java
@@ -0,0 +1,28 @@
+package com.sd.oc.webapp.controller;
+
+import com.sd.oc.serviceproxy.generated.borrowingAPI.BorrowingServiceAPI;
+import com.sd.oc.serviceproxy.generated.userAPI.User;
+import com.sd.oc.serviceproxy.generated.userAPI.UserServiceAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BorrowingController {
+
+    @Autowired
+    UserServiceAPI userServiceAPI;
+
+    @Autowired
+    BorrowingServiceAPI borrowingServiceAPI;
+
+    @GetMapping("/borrowings")
+    public String getBorrowings(Model model){
+        User user= userServiceAPI.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        model.addAttribute("listBorrowings",borrowingServiceAPI.findAllBorrowingOfUser(user.getUserId()));
+        return "borrowings";
+
+    }
+}
