6cb37d3ceec23ce683cb6d44eba87957160055bc
watslink <seb.decroix@gmail.com>
2019-05-28 16:28:00 +0200

    modification prorpietes mailSender

--- bibliowebclient-batch/src/main/java/com/sd/oc/batch/ConfigurationBatch.java
+++ bibliowebclient-batch/src/main/java/com/sd/oc/batch/ConfigurationBatch.java
@@ -83,6 +83,7 @@ public class ConfigurationBatch {
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp" );
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.auth", true );
         props.put("mail.smtp.starttls.enable", true);
         props.put("mail.debug", true);
