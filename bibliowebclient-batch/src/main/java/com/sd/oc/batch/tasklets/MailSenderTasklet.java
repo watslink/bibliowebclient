22119e5cb895dce871a7fd8c34e80c979513a55b
watslink <seb.decroix@gmail.com>
2019-06-11 15:36:03 +0200

    modification du MaiSenderTasklet

--- bibliowebclient-batch/src/main/java/com/sd/oc/batch/tasklets/MailSenderTasklet.java
+++ bibliowebclient-batch/src/main/java/com/sd/oc/batch/tasklets/MailSenderTasklet.java
@@ -3,6 +3,7 @@ package com.sd.oc.batch.tasklets;
 import com.sd.oc.batch.EmailService.EmailService;
 
 
+import com.sd.oc.serviceproxy.generated.borrowingAPI.Borrowing;
 import com.sd.oc.serviceproxy.generated.borrowingAPI.BorrowingServiceAPI;
 import com.sd.oc.serviceproxy.generated.userAPI.User;
 import com.sd.oc.serviceproxy.generated.userAPI.UserServiceAPI;
@@ -35,24 +36,22 @@ public class MailSenderTasklet implements Tasklet {
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
         System.out.println("Batch execute");
 
-//        List<User> listUser=userServiceAPI.findAllUsers();
-//        for (User user:listUser) {
-//
-//        }
-//        List<Borrowing> borrowingsWhoNeedRecallList=borrowingServiceAPI.findAllBorrowingOutOfTime();
-//
-//        for(Borrowing borrowing: borrowingsWhoNeedRecallList){
-//            String to=borrowing.getUser().getMail();
-//            String subject="Fin de prêt";
-//            String text="Bonjour "+borrowing.getUser().getUsername()+","+ls
-//                    +"Merci de venir rendre rapidement ce(s) livre(s) à la bibliothéque:"+ls
-//                    +"- "+borrowing.getBook().getTitle()+" ("+borrowing.getBook().getAuthor()+")"+ls
-//                    +ls+ls
-//                    +"Cordialement,"+ls
-//                    +"BiblioOC.";
-//
-//            emailService.sendSimpleMessage(to, subject, text);
-//        }
+        List<User> listUser=userServiceAPI.findAllUsers();
+        for (User user:listUser) {
+            List<Borrowing> borrowingsWhoNeedRecallList=borrowingServiceAPI.findAllBorrowingOutOfTimeOfUser(user.getUserId());
+            if(borrowingsWhoNeedRecallList!=null){
+                String to=user.getMail();
+                String subject="Fin de prêt";
+                StringBuilder text= new StringBuilder("Bonjour " + user.getUsername() + "," + ls+ls
+                        + "Merci de venir rendre rapidement ce(s) livre(s) à la bibliothéque:" + ls);
+                for(Borrowing borrowing: borrowingsWhoNeedRecallList){
+                    text.append("- ").append(borrowing.getBook().getTitle()).append(" (").append(borrowing.getBook().getAuthor()).append(")").append(ls);
+                }
+                text.append(ls).append(ls).append("Cordialement,").append(ls).append("BiblioOC.");
+
+                emailService.sendSimpleMessage(to, subject, text.toString());
+            }
+        }
         return RepeatStatus.FINISHED;
     }
 }
